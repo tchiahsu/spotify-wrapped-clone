f33d5227a76b146b25f182bacce6796a37031607
Tony Hsu <hsutai.c@gmail.com>
2025-10-05 09:51:11 -0400

    initial next.js create app and spotify pkce test

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center sm:items-center">
+      {/* Page Title */}
+      <div className="flex flex-col justify-center items-center">
+        <div>Your Year In Music</div>
+        <div>A journey through #NUMBER# minutes of sound, #NUMBER# unique tracks, and countless moments</div>
+      </div>
+
+      {/* Top Artists and Tracks */}
+      {/* Musical DNA */}
+      {/* Discovery Journey */}
+      {/* Musical Era */}
+      {/* Moments Reel */}      
+    </main>
+  );
+}
